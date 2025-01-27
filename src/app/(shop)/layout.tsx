d4c8fa747cf20c32fc134e0c135c4493c84ced55
Rocio Buono <rociobuono@hotmail.com>
2025-01-27 17:45:35 -0300

    menu lateral

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,11 +1,12 @@
-import { TopMenu } from "@/components";
+import { Sidebar, TopMenu } from "@/components";
 
 export default function ShopLayout({ children }: {
     children: React.ReactNode;
 }) {
     return (
         <main className="min-h-screen">
-            <TopMenu></TopMenu>
+            <TopMenu/>
+            <Sidebar/>
             <div className="px-0 sm:px-10">
                 {children}
             </div>
