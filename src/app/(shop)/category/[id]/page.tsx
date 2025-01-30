95b20a2d69a81d5f52c65c20c341de0c42689451
Rocio Buono <rociobuono@hotmail.com>
2025-01-30 13:31:17 -0300

    paginas por categorias hechas

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,19 +1,63 @@
+import { ProductGrid } from "@/components/products/product-grid/ProductGrid";
+import { Title } from "@/components/ui/title/Title";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
     params: {
-        id: string;
+        id: Category;
     }
 }
 
+const products = initialData.products;
+
 export default function ({ params }: Props) {
+    
     const { id } = params;
-    if (id === 'kids') {
-        notFound();
+    const productsFiltrados = initialData.products.filter(product => product.gender === id);
+
+    switch (id) {
+        case 'kid':
+            return (<>
+                <Title
+                    title="Tienda"
+                    subtitle="Productos de Niños"
+                    className="mb-2"
+                />
+
+                <ProductGrid
+                    products={productsFiltrados}
+                />
+
+            </>);
+        case 'women':
+            return (<>
+                <Title
+                    title="Tienda"
+                    subtitle="Productos de Mujer"
+                    className="mb-2"
+                />
+
+                <ProductGrid
+                    products={productsFiltrados}
+                />
+
+            </>);
+        case 'men':
+            return (<>
+                <Title
+                    title="Tienda"
+                    subtitle="Productos de Hombre"
+                    className="mb-2"
+                />
+
+                <ProductGrid
+                    products={productsFiltrados}
+                />
+
+            </>);
+        default: break;
     }
-    return (
-        <div>
-            <h1>Category Page {id} </h1>
-        </div>
-    )
+  
 }
